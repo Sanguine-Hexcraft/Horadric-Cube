8300184de99091fcf226bdc2faf6f6edd555e1fa
Sanguine-Hexcraft <hexcraft.apothecary@gmail.com>
2026-03-31 14:19:41 -0600

    renamed node_delimiter to inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -25,3 +25,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                         new_nodes.append(TextNode(part, text_type))
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return text
+
+text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+print(extract_markdown_images(text))
+# [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]    
